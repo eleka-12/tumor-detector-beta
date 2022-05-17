a2de2aa645ffc06531264249ce0aa9c372102113
Karthik-VG <karthikvg.engineer@gmail.com>
2022-05-17 19:44:33 +0530

    adding changes to repo

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
-from flask import Flask
+from urllib import request
+from flask import Flask,flash,request,render_template,redirect
+from flask_cors import CORS,cross_origin
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
-
+uploads = os.getcwd()+"//files//"
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return "Flask app is running"
 
+@app.route("/upload", methods = ['POST','GET'])
+def upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('no file')
+            return redirect(request.url)
+        file = request.files['file']
+    
+        if file.filename == '':
+            print('no filename')
+            return redirect(request.url)
+
+        else:
+
+            filename = secure_filename(file.filename)
+            file.save(uploads+"/"+filename)
+            
+            return redirect('/')
+
+    return render_template('upload.html')
+
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
+    #app.run()
