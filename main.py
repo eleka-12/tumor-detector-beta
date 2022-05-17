026484ef01a9d94c30a8173c8137c649336195d5
Karthik-VG <karthikvg.engineer@gmail.com>
2022-05-17 20:32:48 +0530

    adding api to main.py

--- main.py
+++ main.py
@@ -10,12 +10,46 @@ from flask import Flask, session, request, Response, jsonify
 import atexit
 import uuid
 import os
+
+from urllib import request
+from flask import Flask,flash,request,render_template,redirect
+from flask_cors import CORS,cross_origin
+from werkzeug.utils import secure_filename
+import os
+
+
 app = Flask(__name__)
+uploads = os.getcwd()+"//files//"
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return "Welcome eleinsider.ai team , The top is near...!"
+
+
+app.route("/upload", methods = ['POST','GET'])
+def upload():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('no file')
+            return redirect(request.url)
+        file = request.files['file']
+    
+        if file.filename == '':
+            print('no filename')
+            return redirect(request.url)
+
+        else:
+
+            filename = secure_filename(file.filename)
+            file.save(uploads+"/"+filename)
+            
+            return redirect('/')
+
+    return render_template('upload.html')
+
+
+
 
 port = int(os.getenv("PORT", 5001))
 
